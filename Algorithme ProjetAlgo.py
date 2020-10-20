5aca6fcdc33a2e521d850a75f9faed24129ea3b1
Patrick-Mu <72786552+Patrick-Mu@users.noreply.github.com>
2020-10-20 18:39:18 +0200

    Ajout de document

---
+++ Algorithme ProjetAlgo.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 20 11:03:30 2020
+
+@author: mupat
+"""
+
+f = open("EIVP_KM.csv","r")
+ligne = f.readline()
+f.close()
+
+def colonne(n):
+    f = open("EIVP_KM.csv","r")
+    liste = []
+    f.readline()
+    while f.readline() != '':
+        ligne = f.readline()
+        #fonction qui les points virgules qu'on veut 
+        
+        d
+        
+        
+    
+    
